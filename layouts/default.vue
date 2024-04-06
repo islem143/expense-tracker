0b11ec3599a87014e16d8dd88dddfbf6f8c28499
islem143 <djennaneislempro@gmail.com>
2024-04-06 17:08:51 +0100

    init

---
+++ layouts/default.vue
@@ -0,0 +1,142 @@
+<template>
+  <div class="flex min-h-screen w-full flex-col">
+    <header class="sticky top-0 flex h-16 items-center gap-4 border-b bg-background px-4 md:px-6">
+      <nav class="hidden flex-col gap-6 text-lg font-medium md:flex md:flex-row md:items-center md:gap-5 md:text-sm lg:gap-6">
+        <a class="flex items-center gap-2 text-lg font-semibold md:text-base">
+          <DropdownMenu>
+    <DropdownMenuTrigger as-child>
+      <Button variant="outline">
+        <Icon icon="radix-icons:moon" class="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
+        <Icon icon="radix-icons:sun" class="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+        <span class="sr-only">Toggle theme</span>
+      </Button>
+    </DropdownMenuTrigger>
+    <DropdownMenuContent align="end">
+      <DropdownMenuItem @click="colorMode.preference = 'light'">
+        Light
+      </DropdownMenuItem>
+      <DropdownMenuItem @click="colorMode.preference = 'dark'">
+        Dark
+      </DropdownMenuItem>
+      <DropdownMenuItem @click="colorMode.preference = 'system'">
+        System
+      </DropdownMenuItem>
+    </DropdownMenuContent>
+  </DropdownMenu>
+        </a>
+
+        
+        <a
+          href="#"
+          class="text-foreground transition-colors hover:text-foreground"
+        >
+        <NuxtLink to="/">Dashboard</NuxtLink>
+        </a>
+        <a
+          href="#"
+          class="text-muted-foreground transition-colors hover:text-foreground"
+        >
+        <NuxtLink to="/transactions">Transactions</NuxtLink>
+        </a>
+        <a
+          href="#"
+          class="text-muted-foreground transition-colors hover:text-foreground"
+        >
+          Categories
+        </a>
+        <a
+          href="#"
+          class="text-muted-foreground transition-colors hover:text-foreground"
+        >
+          Wallets
+        </a>
+        <a
+          href="#"
+          class="text-muted-foreground transition-colors hover:text-foreground"
+        >
+          Budget
+        </a>
+      </nav>
+      <Sheet>
+        <SheetTrigger as-child>
+          <Button
+            variant="outline"
+            size="icon"
+            class="shrink-0 md:hidden"
+          >
+            <Menu class="h-5 w-5" />
+            <span class="sr-only">Toggle navigation menu</span>
+          </Button>
+        </SheetTrigger>
+        <SheetContent side="left">
+          <nav class="grid gap-6 text-lg font-medium">
+            <a
+              href="#"
+              class="flex items-center gap-2 text-lg font-semibold"
+            >
+              <Package2 class="h-6 w-6" />
+              <span class="sr-only">Acme Inc</span>
+            </a>
+            <a href="#" class="hover:text-foreground">
+              Dashboard
+            </a>
+            <a
+              href="#"
+              class="text-muted-foreground hover:text-foreground"
+            >
+              Orders
+            </a>
+            <a
+              href="#"
+              class="text-muted-foreground hover:text-foreground"
+            >
+              Products
+            </a>
+            <a
+              href="#"
+              class="text-muted-foreground hover:text-foreground"
+            >
+              Customers
+            </a>
+            <a
+              href="#"
+              class="text-muted-foreground hover:text-foreground"
+            >
+              Analytics
+            </a>
+          </nav>
+        </SheetContent>
+      </Sheet>
+      <div class="flex w-full items-center gap-4 md:ml-auto md:gap-2 lg:gap-4">
+        <form class="ml-auto flex-1 sm:flex-initial">
+          <div class="relative">
+            <Search class="absolute left-2.5 top-2.5 h-4 w-4 text-muted-foreground" />
+            <Input
+              type="search"
+              placeholder="Search products..."
+              class="pl-8 sm:w-[300px] md:w-[200px] lg:w-[300px]"
+            />
+          </div>
+        </form>
+        <DropdownMenu>
+          <DropdownMenuTrigger as-child>
+            <Button variant="secondary" size="icon" class="rounded-full">
+              <CircleUser class="h-5 w-5" />
+              <span class="sr-only">Toggle user menu</span>
+            </Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent align="end">
+            <DropdownMenuLabel>My Account</DropdownMenuLabel>
+            <DropdownMenuSeparator />
+            <DropdownMenuItem>Settings</DropdownMenuItem>
+            <DropdownMenuItem>Support</DropdownMenuItem>
+            <DropdownMenuSeparator />
+            <DropdownMenuItem>Logout</DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
+      </div>
+    </header>
+
+    <slot />
+  </div>
+</template>
---
+++ nuxt.config.ts
@@ -0,0 +1,17 @@
+export default defineNuxtConfig({
+  modules: ['@nuxtjs/tailwindcss', 'shadcn-nuxt', '@nuxtjs/color-mode'],
+  colorMode: {
+    classSuffix: ''
+  },
+  shadcn: {
+    /**
+     * Prefix for all the imported component
+     */
+    prefix: '',
+    /**
+     * Directory that the component lives in.
+     * @default "./components/ui"
+     */
+    componentDir: './components/ui'
+  }
+})
